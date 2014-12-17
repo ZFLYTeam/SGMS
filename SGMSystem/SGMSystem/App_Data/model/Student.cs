4cf2c1f743dab75583c784f3b7fed2aaafca9d10
张嘉 <ihueij1993@gmail.com>
2014-12-17 12:45:57 +0800

    添加Student 和 teacher实体类

---
+++ SGMSystem/SGMSystem/App_Data/model/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SGMSystem
+{
+    /// <summary>
+    /// 作者:张嘉
+    /// 学生对象实体类
+    /// </summary>
+    public class Student
+    {
+        public int id;              //id
+        public String name;         //名字
+        public String sno;          //学号
+        public String sex;          //性别
+        public String birth;        //出生年月
+        public String nation;       //民族
+        public String political;    //政治面貌
+        public String headImage;    //头像地址
+        public int classId;         //班级id
+        public String idNum;        //身份证号
+        public String password;     //登录密码
+    }
+}
---
+++ SGMSystem/SGMSystem/App_Data/model/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SGMSystem
+{
+    /// <summary>
+    /// 教师对象实体类
+    /// </summary>
+    public class Teacher
+    {
+        public int id;              //id
+        public String techNum;      //职工号
+        public String TeacherName;  //老师名字
+        public String sex;          //性别
+        public String birth;        //出生年月
+        public String title;        //职称
+        public String headImage;    //头像地址
+        public String password;     //密码
+    }
+}
